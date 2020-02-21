7be7f1b5280ec940ac66559a337b23486e7ad7a6
mike <mikekrenn11@gmail.com>
2020-02-21 16:19:04 +0100

    End form Edited

--- src/main/java/End.java
+++ src/main/java/End.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class End {
+    private JPanel endPanel;
+    private JLabel winnerMSGandName;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        endPanel.setSize(1280,720);
+    }
 }
