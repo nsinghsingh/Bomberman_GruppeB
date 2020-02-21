dc1299e19cea646a39b5ece15bca6fdf01f3b5d0
vmadmin <adrian.p.michel@hotmail.com>
2020-02-22 00:13:57 +0100

    Labyrinth and End Push 1/2

--- src/main/java/End.java
+++ src/main/java/End.java
@@ -1,2 +1,6 @@
+import javax.swing.*;
+
 public class End {
+    private JPanel End;
+    private JPanel Scoreboard;
 }
--- src/main/java/Labyrinth.java
+++ src/main/java/Labyrinth.java
@@ -1,2 +1,19 @@
+import javax.swing.*;
+
 public class Labyrinth {
+    private JPanel Labyrinth;
+    private JPanel GameRender;
+    private JPanel GameInfo;
+    private JLabel Time;
+    private JPanel ClockIcon;
+    private JLabel P1Score;
+    private JLabel P2Score;
+    private JLabel P3Score;
+    private JLabel P4Score;
+    private JLabel TieScore;
+    private JPanel TieIcon;
+    private JPanel P4Icon;
+    private JPanel P3Icon;
+    private JPanel P2Icon;
+    private JPanel P1Icon;
 }
