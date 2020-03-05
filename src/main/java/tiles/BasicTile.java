c4d8fb9f2f79e1841fa23aad99828d6ca4b8c366
nsinghsingh <nihal.singh@csbe.ch>
2020-03-05 10:00:45 +0100

    Somewhat functioning prototype

--- src/main/java/tiles/BasicTile.java
+++ src/main/java/tiles/BasicTile.java
@@ -18,10 +18,9 @@ public class BasicTile extends JPanel{
         BasicTile newTile = new BasicTile();
         newTile.setSolid(isSolid());
         newTile.setDestroyable(isDestroyable());
-        newTile.setImagePath("/sprites/tiles/Grass1.png");
+        newTile.setImagePath(getImagePath());
         newTile.setBackground(getBackground());
         return newTile;
-        //getImagePath()
     }
 
     public void setImagePath(String imagePath) {
--- src/main/java/tiles/Player.java
+++ src/main/java/tiles/Player.java
@@ -14,7 +14,7 @@ public class Player extends BasicTile{
     @Getter @Setter private int targetX = 50;
     @Getter @Setter private int targetY = 50;
     @Getter @Setter private BasicTile[][] tiles;
-    @Getter @Setter private JLabel playerSprite;
+    @Getter @Setter private JLabel playerIcon;
     @Getter @Setter private String direction = "n";
     @Getter @Setter private BasicTile currentTile;
 
@@ -24,11 +24,10 @@ public class Player extends BasicTile{
         setImagePath("");
         setTiles(tiles);
         currentTile = this;
-        URL imgURL = getClass().getResource("sprites/players/black/BStand.png");
-        ImageIcon sprite = new ImageIcon(imgURL);
-        setPlayerSprite(new JLabel(sprite, JLabel.CENTER));
-        playerSprite.setMinimumSize(new Dimension(50, 50));
-        add(playerSprite);
+        URL imgURL = getClass().getResource("../icons/BIcon.png");
+        ImageIcon icon = new ImageIcon(imgURL);
+        setPlayerIcon(new JLabel(icon));
+        add(getPlayerIcon());
     }
 
     @Override
@@ -43,9 +42,10 @@ public class Player extends BasicTile{
                 setDirection(getDirection());
                 int xMovement = (getTargetX() - getXPosition()) / getSize().width;
                 int yMovement = (getTargetY() - getYPosition()) / getSize().height;
-                playerSprite.setLocation(playerSprite.getX() + xMovement, playerSprite.getY() + yMovement);
+                playerIcon.setLocation(playerIcon.getX() + xMovement, playerIcon.getY() + yMovement);
                 setXPosition(getXPosition() + xMovement);
                 setYPosition(getYPosition() + yMovement);
+                System.out.println(getXPosition());
                 BasicTile target = getTiles()[getXPosition()/getSize().width][getYPosition()/getSize().height];
                 if(getCurrentTile() != target){
                     target.setSolid(true);
@@ -92,9 +92,7 @@ public class Player extends BasicTile{
         catch (Exception ignored){}
     }
 
-    public void placeBomb(){
-
-    }
+    public void placeBomb(){ }
 
     public void die(){
 
